f142d9466132cb7fc1058180bb2b6c8887cdeae4
RalphGonzales3 <thirg03@yahoo.co.uk>
2020-12-08 02:32:21 -0800

    Added Stimulus and configuring application to work with it

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   get 'contact', to: 'home#contact'
   post 'request_contact', to: 'home#request_contact'
   get 'basketball', to: 'basketballs#index'
+  get '/basketballs/:basketball_id/discussions', to: 'discussions#index'
 
   root 'home#home'
 end
