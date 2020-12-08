44e55ac51daad61523a93b1a264608c57e3a3681
RalphGonzales3 <thirg03@yahoo.co.uk>
2020-12-07 16:51:10 -0800

    Added proper routing and fixed some scss for Basketball index

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   resources :basketballs
   get 'contact', to: 'home#contact'
   post 'request_contact', to: 'home#request_contact'
+  get 'basketball', to: 'basketballs#index'
 
   root 'home#home'
 end
