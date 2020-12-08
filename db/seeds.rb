67760a39e5fc20bdb21ca8710898ad59a50bb945
RalphGonzales3 <thirg03@yahoo.co.uk>
2020-12-07 16:33:36 -0800

    Seeded basketball db

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+#Basketball.create(name: , team:, facts:)
+Basketball.create(name: "Lebron James", team: "Los Angeles Lakers", facts: "He has won 4 championships in total and is going for more!")
+Basketball.create(name: "Michael Jordan", team: "Chicago Bulls", facts: "He is considered the GOAT of Basketball after winning 6 championships")
+Basketball.create(name: "Stephen Curry", team: "Golden State Warriors", facts: "He holds the record for three-pointers in a season in both the NBA and NCAA")
+Basketball.create(name: "Kobe Bryant", team: "Los Angeles Lakers", facts: "Kobe was the first guard to be drafted by the NBA out of high school")
